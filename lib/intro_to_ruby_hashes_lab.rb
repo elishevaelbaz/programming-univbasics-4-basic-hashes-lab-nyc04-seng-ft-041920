12083a52ff197ef2bae8fd9f53c1c46bdb066298
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-22 20:24:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,8 +3,8 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
-end
+{  # return a valid hash with any key/value pair of your choice
+}end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
